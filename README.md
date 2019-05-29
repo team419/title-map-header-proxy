75cd2f22e1ef456da272fcab4cb3e237c4eaafcb
Daniel Kim <nautilus@Nautilus.local>
2019-05-28 19:52:06 -0700

    Initial commit - structure only, no content

---
+++ README.md
@@ -0,0 +1,4 @@
+# FEC - Team 419 Proxy
+  - Daniel Kim
+
+Repo for combining FEC modules built by Team 419
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Title from '.title-map-header-module/client/src/components/app';
+
+ReactDOM.render(<Title />, document.getElementById('title'));
